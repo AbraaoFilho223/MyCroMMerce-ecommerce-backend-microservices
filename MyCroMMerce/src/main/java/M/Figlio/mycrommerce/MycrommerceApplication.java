902c5ca6e2d9c9d1fcdc2535d2ae1e490e748f88
Abraão Filho <abraaofilho223@hotmail.com>
2025-04-01 15:42:27 -0300

    Initial Spring Boot project setup

---
+++ MyCroMMerce/src/main/java/M/Figlio/mycrommerce/MycrommerceApplication.java
@@ -0,0 +1,13 @@
+package M.Figlio.mycrommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycrommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycrommerceApplication.class, args);
+	}
+
+}
---
+++ MyCroMMerce/src/test/java/M/Figlio/mycrommerce/MycrommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package M.Figlio.mycrommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycrommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
